fce2adbaf831bf6457657d39b5052004f6b48161
jaesekim <jaeseki@gmail.com>
2024-04-12 18:11:49 +0900

    :iphone: Design: 테마 색상 지정

--- UriBoard/UriBoard/Design/ColorStyle.swift
+++ UriBoard/UriBoard/Design/ColorStyle.swift
@@ -8,5 +8,9 @@
 import UIKit
 
 enum ColorStyle {
-    static let black = UIColor.black
+    static let black = UIColor(hexCode: "000000")
+    static let darkBlue = UIColor(hexCode: "14213D")
+    static let darkYellow = UIColor(hexCode: "FCA311")
+    static let gray = UIColor(hexCode: "E5E5E5")
+    static let white = UIColor(hexCode: "FFFFFF")
 }
