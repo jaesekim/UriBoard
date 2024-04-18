dbb3b14bfd11a32e40ed16cba9d8dab9c8e2bc96
jaesekim <jaeseki@gmail.com>
2024-04-18 17:57:03 +0900

    게시글 collectionViewCell 레이아웃 잡기

--- UriBoard/UriBoard/View/Home/BoardCollectionViewCell.swift
+++ UriBoard/UriBoard/View/Home/BoardCollectionViewCell.swift
@@ -34,7 +34,20 @@ class BoardCollectionViewCell: UICollectionViewCell {
         $0.axis = .horizontal
         $0.spacing = 20
     }
-    let likeButton = CustomButton(<#T##image: String##String#>)
+    let likeButton = CustomButton("heart")
+    let commentButton = CustomButton("message")
+    let repeatButton = CustomButton("repeat")
+    
+    let commentTextField = UITextField.addLeftImage(
+        image: "bubble.right.pulse",
+        placeholder: "답글을 입력해 주세요"
+    ).then {
+        $0.font = FontStyle.getFont(
+            scale: .regular,
+            size: .small
+        )
+    }
+    let sendButton = CustomButton("paperplane.bounce")
     // 댓글, 좋아요 개수 보여주는 스택 뷰
     let statusStack = UIStackView().then {
         $0.axis = .horizontal
@@ -75,20 +88,59 @@ extension BoardCollectionViewCell: UISettings {
             nicknameLabel,
             contentLabel,
             buttonStack,
+            commentTextField,
             statusStack,
         ].forEach { contentView.addSubview($0) }
         
         [
+            likeButton,
+            commentButton,
+            repeatButton,
         ].forEach { buttonStack.addArrangedSubview($0) }
         
         [
             commentLabel,
+            likeLabel,
             
         ].forEach { statusStack.addArrangedSubview($0) }
     }
     
     func configureConstraints() {
-        
+        profileImage.snp.makeConstraints { make in
+            make.size.equalTo(44)
+            make.top.leading.equalTo(safeAreaLayoutGuide).offset(16)
+        }
+        nicknameLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImage.snp.top)
+            make.leading.equalTo(profileImage.snp.trailing).offset(16)
+        }
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameLabel.snp.bottom).offset(8)
+            make.height.lessThanOrEqualTo(120)
+            make.left.equalTo(nicknameLabel.snp.leading)
+            make.trailing.equalTo(safeAreaLayoutGuide).offset(-16)
+        }
+        buttonStack.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(8)
+            make.leading.equalTo(contentLabel.snp.leading)
+            make.height.equalTo(40)
+            make.trailing.lessThanOrEqualTo(sendButton.snp.leading)
+        }
+        sendButton.snp.makeConstraints { make in
+            make.size.equalTo(40)
+            make.top.equalTo(buttonStack.snp.top)
+            make.trailing.equalTo(safeAreaLayoutGuide).offset(-16)
+        }
+        statusStack.snp.makeConstraints { make in
+            if buttonStack.isHidden {
+                make.top.equalTo(contentLabel.snp.bottom).offset(8)
+            } else {
+                make.top.equalTo(buttonStack.snp.bottom).offset(8)
+            }
+            make.leading.equalTo(contentLabel.snp.leading)
+            make.trailing.lessThanOrEqualTo(contentView.snp.trailing)
+            make.height.equalTo(20)
+        }
     }
     
     func configureView() {
