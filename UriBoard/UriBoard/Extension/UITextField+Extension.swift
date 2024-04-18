31ff2f0458473a599a55442685fc07e9736d389a
jaesekim <jaeseki@gmail.com>
2024-04-18 17:57:30 +0900

    :iphone: Design: TextField 디자인

--- UriBoard/UriBoard/Extension/UITextField+Extension.swift
+++ UriBoard/UriBoard/Extension/UITextField+Extension.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UITextField {
     
-    static func authTextField(placeholder: String) -> UITextField {
+    static func addLeftPadding(placeholder: String) -> UITextField {
         
         let view = UITextField()
         
@@ -17,6 +17,28 @@ extension UITextField {
             frame: CGRect(x: 0, y: 0, width: 20, height: view.frame.height)
         )
         view.leftView = paddingView
+        view.leftViewMode = .always
+        view.layer.cornerRadius = 8
+        view.clipsToBounds = true
+        view.layer.borderWidth = 1
+        view.layer.borderColor = ColorStyle.darkYellow.cgColor
+        view.placeholder = placeholder
+
+        return view
+    }
+    
+    static func addLeftImage(
+        image: String,
+        placeholder: String
+    ) -> UITextField {
+        
+        let view = UITextField()
+        
+        let leftImage = UIView(
+            frame: CGRect(x: 0, y: 0, width: view.frame.height, height: view.frame.height)
+        )
+        
+        view.leftView = leftImage
         view.leftViewMode = .always
         view.layer.cornerRadius = 8
         view.clipsToBounds = true
