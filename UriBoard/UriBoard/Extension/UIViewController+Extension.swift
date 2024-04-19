327ccc9e21bdb088fe9c990a4de4b5ce89462011
jaesekim <jaeseki@gmail.com>
2024-04-19 15:02:06 +0900

    :sparkles: Feat: root view 변경 extension 생성

---
+++ UriBoard/UriBoard/Extension/UIViewController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Extension.swift
+//  UriBoard
+//
+//  Created by 김재석 on 4/19/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func rootViewTransition() {
+
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+
+        let sceneDelegate = windowScene.delegate as? SceneDelegate
+
+        sceneDelegate?.window?.rootViewController = TabBarController()
+        sceneDelegate?.window?.makeKey()
+    }
+}
